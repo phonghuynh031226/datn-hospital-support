54737982dd07b69c73f55d0c17513392df34659e
nhutdady <minhnhut261006@gmail.com>
2026-07-02 09:29:59 +0700

    code nhập xuất kho điều chỉnh MedicineCategory và đã hoàn thành kho thuốc

--- Backend/hospital_support/src/main/java/com/poly/hospital_support/entity/Medicine.java
+++ Backend/hospital_support/src/main/java/com/poly/hospital_support/entity/Medicine.java
@@ -8,6 +8,7 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Medicine {
 
     @Id
--- Backend/hospital_support/src/main/java/com/poly/hospital_support/entity/MedicineCategory.java
+++ Backend/hospital_support/src/main/java/com/poly/hospital_support/entity/MedicineCategory.java
@@ -1,4 +1,21 @@
 package com.poly.hospital_support.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "medicine_category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MedicineCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String tenLoai;
+
+    private String moTa;
 }
